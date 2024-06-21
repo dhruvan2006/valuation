2dbf0b06eecdbe78ebe107ed3f9c532ce9458b5f
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-06-21 20:45:19 +0200

    Add bitcoin to valuation api

--- backend/routes/valuation.js
+++ backend/routes/valuation.js
@@ -13,9 +13,9 @@ const getTimestamp = () => {
 let storedData = {};
 let combinedData = {};
 
-async function fetchCheckOnChain(api_url, indicator_column_name) {
+async function fetchCheckOnChain(url, indicator_column_name, storedData, combinedData) {
     try {
-        const response = await axios.get(api_url);
+        const response = await axios.get(url);
         const htmlContent = response.data;
 
         // Load HTML content into Cheerio for parsing
@@ -53,23 +53,60 @@ async function fetchCheckOnChain(api_url, indicator_column_name) {
                             values: values,
                             zScores: zScores
                         };
-                        console.log(`{${getTimestamp()}} Fetched data for ${api_url}`)
+                        console.log(`{${getTimestamp()}} Fetched data for ${url}`)
                     }
                 });
             } else {
                 throw new Error('JSON data not found within the Plotly script');
             }
         } else {
-            throw new Error(`Plotly script not found on webpage ${api_url}`);
+            throw new Error(`Plotly script not found on webpage ${url}`);
         }
     } catch (error) {
-        console.error(`{${getTimestamp()}} Error processing indicator: ${error}`);
+        console.error(`{${getTimestamp()}} Error processing checkonchain: ${error}`);
     }
-};
+}
+
+let d = [];
+
+async function fetchWoocharts(indicator_column_name) {
+    try {
+        const response = await axios.get("https://woocharts.com/bitcoin-macro-oscillator/data/chart.json?1718986908477");
+        const json_data = response.data;
+
+        // const col = indicator_column_name;
+        // const x = json_data.col.x;
+        // const y = json_data.co.y;
+
+        d = json_data;
+    } catch (error) {
+        console.error(`{${getTimestamp()}} Error processing woocharts: ${error}`);
+    }
+}
+
+// Fetch woocharts
+fetchWoocharts("price");
+
+// Fetch checkonchain indicators
 indicators = [['https://charts.checkonchain.com/btconchain/pricing/pricing_mvrv_aviv_zscore/pricing_mvrv_aviv_zscore_light.html', 'AVIV Z-Score'],
-              ['https://charts.checkonchain.com/btconchain/pricing/pricing_mayermultiple_zscore/pricing_mayermultiple_zscore_light.html', 'Price'],
+              ['https://charts.checkonchain.com/btconchain/realised/realised_sopr/realised_sopr_light.html', 'SOPR 7D-EMA'],
+              ['https://charts.checkonchain.com/btconchain/mining/mining_difficultyregression/mining_difficultyregression_light.html', 'Difficulty Multiple'],
+              ['https://charts.checkonchain.com/btconchain/lifespan/lifespan_vddmultiple/lifespan_vddmultiple_light.html', 'VDD Multiple'],
               ['https://charts.checkonchain.com/btconchain/pricing/pricing_mayermultiple_zscore/pricing_mayermultiple_zscore_light.html', 'Mayer Multiple Z']]
-indicators.forEach(([ url, indicator ]) => fetchCheckOnChain(url, indicator));
+indicators.forEach(([ url, indicator ]) => fetchCheckOnChain(url, indicator, storedData, combinedData));
+
+// Fetch bitcoin price
+bitcoinData = {}
+bitcoinCombined = {}
+fetchCheckOnChain('https://charts.checkonchain.com/btconchain/pricing/pricing_mayermultiple_zscore/pricing_mayermultiple_zscore_light.html', 'Price', bitcoinData, bitcoinCombined)
+
+router.get('/bitcoin', (req, res) => {
+    res.json(bitcoinData.Price);
+});
+
+router.get('/hi', (req, res) => {
+    res.send(d);
+});
 
 router.get('/', (req, res) => {
     let averagedData = {};
